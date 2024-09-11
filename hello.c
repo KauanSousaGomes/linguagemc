98ed6cd75fc789ba04c3330b905c81f19e44a689
kauan <kauangames2004@gmail.com>
2024-09-11 11:50:03 +0000

    first commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+// Declaração de Biblioteca padrao de entrada e saida de dados
+// std (standard) i (input) o (output) 
+#include <stdio.h>  
+// Inicio do Programa.           
+int main(){
+// Comando para exibir o texto hello word na tela
+//o comando printf so executa porque voce fez a importação da biblioteca stdio 
+ printf("Hello Word");
+
+    return 0;
+}
